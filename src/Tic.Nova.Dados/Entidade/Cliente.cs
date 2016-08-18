0aae7e35aefbce008ec9b95e315089a903d00e29
Zoio Silva <zoiosilva@yahoo.com.br>
2016-08-18 17:04:33 -0300

    Adaptando nome da tabela para bater com usuário da mv.

--- src/Tic.Nova.Dados/Entidade/Cliente.cs
+++ src/Tic.Nova.Dados/Entidade/Cliente.cs
@@ -8,7 +8,7 @@ namespace Tic.Nova.Dados.Entidade
 {
     public class Cliente : TableEntity
     {
-        public static string NomeTabela = "Cliente0";
+        public static string NomeTabela = "Cliente5";
 
         public Cliente() { }
 
